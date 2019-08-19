9900f7aaac5d4c4352bea0fe4e53a537760ac70b
Jinesh Patel <jineshp.etechmavens@gmail.com>
2019-08-19 12:20:23 +0530

    Added test cases

---
+++ PodTestObjectiveC/TestVC.h
@@ -0,0 +1,17 @@
+//
+//  TestVC.h
+//  PodTestObjectiveC
+//
+//  Created by etech-dev on 8/19/19.
+//  Copyright © 2019 etech-dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PodTestObjectiveC/TestVC.m
@@ -0,0 +1,37 @@
+//
+//  TestVC.m
+//  PodTestObjectiveC
+//
+//  Created by etech-dev on 8/19/19.
+//  Copyright © 2019 etech-dev. All rights reserved.
+//
+
+#import "TestVC.h"
+
+@interface TestVC ()
+
+@end
+
+@implementation TestVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)testFunc
+{
+    NSLog(@"This is test func");
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
